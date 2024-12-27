4c4163df692776bbc94f21d2a140d59b339855dc
idibyaranjan15 <dibyaranjansahoo696@gmail.com>
2024-12-27 12:46:39 +0530

    upading message controller section

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectDb from "./db/connectDb.js";
 import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(5001, () => {
   connectDb();
